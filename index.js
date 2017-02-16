992e7d41c9d9ebabedbacd34ea471537955a420e
Jean Yang <bdlas321@gmail.com>
2017-02-16 10:51:18 +0800

    set up vhost

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const vhost = require('vhost');
 
 const app = express();
+const PORT = 3001;
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Api home');
+});
+
+router.get('/login', (req, res) => {
+  res.send('login api');
+});
+
+app.use(vhost('api.localhost', router));
 
 app.get('/', (req, res) => {
   res.send('Home route!');
@@ -10,6 +24,6 @@ app.get('/dashboard', (req, res) => {
   res.send('This is a dashboard.');
 })
 
-app.listen(3001, function () {
-  console.log('Listening on port 3001!')
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}!`)
 });
